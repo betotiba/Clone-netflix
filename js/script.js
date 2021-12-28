203156ab3da4d58ed4e0194f7aba923b12a87a07
roberto.zbo <roberto.zbo@gmail.com>
2021-12-28 14:54:34 -0300

    Adicionado

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(document).on('click', '#add_favoritos', function(){
+   alert("Vídeo adicionado a seus favoritos ");
+   document.getElementById("favorito").innerHTML = "Sucesso! Adicionados aos meus favoritos!";
+
+});
